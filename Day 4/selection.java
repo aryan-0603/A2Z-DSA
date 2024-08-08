0e13b84cca507e09c4b73205c63ccc7aa9a50c75
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2024-08-08 13:50:45 +0530

    Update selection.java

--- Day 4/selection.java
+++ Day 4/selection.java
@@ -4,12 +4,14 @@ public class selection {
         int n=10;
         int[] arr=new int[n];
         Random ran=new Random();
+        System.out.println("Original array:");
         for(int i=0;i<10;i++){
             arr[i]=ran.nextInt(100);
+            System.out.print(arr[i]+" ");
         }
-        for (int i = 0; i < n-2; i++) {
+        for (int i = 0; i <= n-2; i++) {
             int min=i;
-            for(int j=i;j<n-1;j++){
+            for(int j=i;j<=n-1;j++){
                 if(arr[j]<arr[min]){
                     min=j;
                 }
@@ -17,6 +19,10 @@ public class selection {
             int temp=arr[i];
             arr[i]=arr[min];
             arr[min]=temp;            
+        }
+        System.out.println("\nAfter swap:");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(arr[i]+" ");
         }
     }
 }
