7da77a3e6d80d5045acb57037a1bf6203d2c847f
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2024-08-13 23:33:15 +0530

    Create rotatebyK.java

---
+++ Day 7/rotatebyK.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+public class rotatebyK {
+    public static void rotate(int arr[],int n){
+        int temp=arr[0];
+        for (int i = 0; i < n-1; i++) {
+            arr[i]=arr[i+1];
+        }
+        arr[n-1]=temp;
+    }
+    public static void main(String args[]){
+    int n = 10;
+        int[] arr = new int[n];
+        Random ran = new Random();
+        System.out.println("Original array:");
+        for (int i = 0; i < 10; i++) {
+            arr[i] = ran.nextInt(40);
+            System.out.print(arr[i] + " ");
+        }
+        rotate(arr,n);
+        System.out.println("\nAfter insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
