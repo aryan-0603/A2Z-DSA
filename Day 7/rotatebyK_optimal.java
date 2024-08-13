f11bb41356601813ecad8d4b1fb1cfa87d81c193
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2024-08-14 00:53:56 +0530

    Create rotatebyK_optimal.java

---
+++ Day 7/rotatebyK_optimal.java
@@ -0,0 +1,34 @@
+import java.util.Random;
+
+public class rotatebyK_optimal {
+    public static void reverse(int arr[],int start,int end) {
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(int arr[],int n,int d) {
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+    public static void main(String args[]){
+     int n = 10;
+        int[] arr = new int[n];
+        Random ran = new Random();
+        System.out.println("Original array:");
+        for (int i = 0; i < 10; i++) {
+            arr[i] = ran.nextInt(40);
+            System.out.print(arr[i] + " ");
+        }
+        int d=ran.nextInt(10);
+        rotate(arr,n,d);
+        System.out.println("\nAfter rotation by "+d+": ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
