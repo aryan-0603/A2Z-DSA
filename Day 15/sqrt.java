74d6b1fd8a5d4b7d5683fe48e3ed4c665245cb54
aryan-0603 <114023565+aryan-0603@users.noreply.github.com>
2024-09-03 14:36:44 +0530

    Update sqrt.java

--- Day 15/sqrt.java
+++ Day 15/sqrt.java
@@ -1,1 +1,19 @@
-
+public class sqrt {
+    public static int floorSqrt(int n){
+        long low=1,high=n;
+        while(low<=high){
+            long mid=(low+high)/2;
+            long value=mid*mid;
+            if(value<=(long)n)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+        return (int)high;
+    }
+    public static void main(String[] args) {
+        int n = 288;
+        int ans = floorSqrt(n);
+        System.out.println("The floor of square root of " + n + " is: " + ans);
+    }
+}
